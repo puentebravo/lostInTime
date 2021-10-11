49588b0e6b936b6429c0f3df042cc35de6402e59
Puentebravo <puentematos@gmail.com>
2021-10-11 18:37:10 -0400

    configured mySQL database connection, sync.

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
+// @ts-check
+
 const express = require("express");
-const path = reqruire("path");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require("./models");
+const config = require("./config/config");
+const { Sequelize } = require("sequelize");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: "mysql",
+  }
+);
+
+try {
+  sequelize.authenticate();
+  console.log("Connection Online.");
+} catch (error) {
+  console.log(`Connection failed on: ${error}`);
+}
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
@@ -13,6 +38,6 @@ app.get("*", function (req, res) {
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server now listening on ${PORT}. Check localhost.`);
+    console.log(`Server now listening on ${PORT}. Check localhost.`, PORT);
   });
 });
