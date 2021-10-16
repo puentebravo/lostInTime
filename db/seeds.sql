79138940c5cfceff2ccf213a437cf5d9abd003f7
Puentebravo <puentematos@gmail.com>
2021-10-16 11:41:06 -0400

    modified test seed insert statement

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
-INSERT INTO POSTS (content, tags, createdAt, updatedAt)
-VALUES ("This post is a test. It is only a test. There are roads yet to be traveled.", "test", now(), now())
+INSERT INTO POSTS (content, tags)
+VALUES ("This post is a test. It is only a test. There are roads yet to be traveled.", "test")
