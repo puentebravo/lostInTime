660083c1bd2daa2413bfa5bc5599e26f39454b4f
Puentebravo <puentematos@gmail.com>
2021-10-30 14:02:55 -0400

    Scaffolded out component structure, added base code to navbar/footer

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Footer() {
+    return (
+        <div className="container mx-auto object-bottom fixed bg-gray-700 text-gray-200 text-sm font-light">
+            <p className="left-0">Copyright 2021 Lost in Time/ Maya Caballero. Site created by Charlie Puente</p>
+        </div>
+    )
+}
+
+
+export default Footer
---
+++ client/src/components/navBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div className="container items-center">
+            <p>Placeholder text.</p>
+        </div>
+    )
+}
+
+export default NavBar
