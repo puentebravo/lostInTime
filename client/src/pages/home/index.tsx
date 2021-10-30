74c4697abf077802b0b01d03950b5172246def37
Puentebravo <puentematos@gmail.com>
2021-10-30 14:30:20 -0400

    added page components

---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navBar";
+
+function Home() {
+    return (
+        <div>
+        <Navbar />
+        <Footer />
+        </div>
+    )
+}
+
+export default Home
