1da7d469b8f347132f4c249feb5a188986dec8c1
Puentebravo <puentematos@gmail.com>
2021-11-06 10:44:03 -0400

    Pinned footer to bottom of viewport, centered text.

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="container mx-auto object-bottom fixed bg-gray-700 text-gray-200 text-sm font-light">
-            <p className="left-0">Copyright 2021 Lost in Time/ Maya Caballero. Site created by Charlie Puente</p>
+        <div className="container mx-auto h-8 inset-x-0 bottom-0 position: absolute bg-gray-700 text-gray-200 text-sm font-light ">
+            <p className="font-sans text-center">Copyright 2021 Lost in Time/ Maya Caballero. Site created by Charlie Puente</p>
         </div>
     )
 }
