60e59361af86b67db3a4ca966b8b4998398400fa
Puentebravo <puentematos@gmail.com>
2021-11-06 21:04:30 -0400

    Added placeholder blocks to navbar

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+// List is a placeholder. Use grid/flexbox to make each link a button with hover over functionality. 
+
 function NavBar() {
     return (
-        <div className="container items-center">
-            <p>Placeholder text.</p>
+        <div className="container mx-auto h-auto position: sticky top-0 bg-gray-700">
+            <p className="text-center text-gray-300">Placeholder Title</p>
+            <ul className="text-center text-gray-300 list-none">
+                <li >Home</li>
+                <li >About</li>
+                <li >Blog</li>
+                <li >Contact</li>
+            </ul>
+
         </div>
     )
 }
