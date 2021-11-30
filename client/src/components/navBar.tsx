998bee3bab157576b5a6af2cc4dc55c9fc448b53
Puentebravo <puentematos@gmail.com>
2021-11-30 17:08:12 -0500

    fixed Navbar, added hover animations.

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -4,14 +4,15 @@ import React from "react";
 
 function NavBar() {
     return (
-        <div className="container mx-auto h-auto position: sticky top-0 bg-gray-700">
-            <p className="text-center text-gray-300">Placeholder Title</p>
-            <ul className="text-center text-gray-300 list-none">
-                <li >Home</li>
-                <li >About</li>
-                <li >Blog</li>
-                <li >Contact</li>
-            </ul>
+        <div className="container mx-auto h-auto position: sticky top-0 bg-yellow-400">
+            <p className="text-center text-3xl">Lost in Time</p>
+            <div className="grid grid-cols-4 justify-center">
+                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Home</div>
+                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Blog</div>
+                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">About</div>
+                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Contact</div>
+
+            </div>
 
         </div>
     )
