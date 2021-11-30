0d896030e03968c9137f30eeddb7977f5ac393ad
Puentebravo <puentematos@gmail.com>
2021-11-30 17:33:10 -0500

    added react router to app.tsx.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/home';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import './App.css';
+
 
 function App() {
   return (
-    <Home />
+    <Router basename={`${process.env.PUBLIC_URL}/`}>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
+    
   );
 }
 
