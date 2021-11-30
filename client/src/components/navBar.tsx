61058e8faf181f5b669bfdd497bc81a301a870fc
Puentebravo <puentematos@gmail.com>
2021-11-30 17:32:48 -0500

    added links to navbar

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-
-// List is a placeholder. Use grid/flexbox to make each link a button with hover over functionality. 
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <div className="container mx-auto h-auto position: sticky top-0 bg-yellow-400">
             <p className="text-center text-3xl">Lost in Time</p>
             <div className="grid grid-cols-4 justify-center">
-                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Home</div>
-                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Blog</div>
+                <Link to="/"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Home</div></Link>
+                <Link to="/blog"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Blog</div></Link>
                 <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">About</div>
-                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Contact</div>
+                <Link to="/contact"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Contact</div></Link>
 
             </div>
 
