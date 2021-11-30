e5baadc5ff32748befcf82fd7c5d3ed2467b80f3
Puentebravo <puentematos@gmail.com>
2021-11-30 17:33:51 -0500

    Added leadcard with sample text to home page.

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../../components/footer";
+import LeadCard from "../../components/leadCard";
 import Navbar from "../../components/navBar";
 
 function Home() {
     return (
         <div>
             <Navbar />
+            <LeadCard />
             <Footer />
         </div>
     )
