a5a9ccfa5f504963302f4c167345fafa6e41d0b6
Puentebravo <puentematos@gmail.com>
2021-12-02 19:57:35 -0500

    Added about page, stylesheet

---
+++ client/src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../components/footer";
+import NavBar from "../../components/navBar";
+
+function About() {
+    return (
+        <div>
+            <NavBar />
+            <Footer />
+        </div>
+    )
+}
+
+export default About
