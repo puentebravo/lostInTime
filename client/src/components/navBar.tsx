f9ae15f436690d54ee6375eb37748acbf3a6a73b
Puentebravo <puentematos@gmail.com>
2021-12-07 18:33:45 -0500

    added link to About page

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -8,7 +8,7 @@ function NavBar() {
             <div className="grid grid-cols-4 justify-center">
                 <Link to="/"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Home</div></Link>
                 <Link to="/blog"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Blog</div></Link>
-                <div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">About</div>
+                <Link to="/about"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">About</div></Link>
                 <Link to="/contact"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Contact</div></Link>
 
             </div>
