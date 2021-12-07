9626c47d23a822fbcc55995d58e0ffde12b8a7ad
Puentebravo <puentematos@gmail.com>
2021-12-07 18:32:33 -0500

    made footer sticky

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="container mx-auto h-auto inset-x-0 bottom-0 position: absolute bg-gray-700 text-gray-200 text-sm font-light ">
+        <div className="container mx-auto h-auto inset-x-0 bottom-0 position: sticky bg-gray-700 text-gray-200 text-sm font-light ">
             <p className="font-sans text-center text-xs my-2">Copyright 2021 Lost in Time/ Maya Caballero. Site created by Charlie Puente</p>
         </div>
     )
