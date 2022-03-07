81e82aa985d6f342a8e56f75fd94cc0764510305
Charlie Puente <puentematos@gmail.com>
2022-03-07 15:36:00 -0500

    Swapped out lorem text.

--- client/src/components/leadCard.tsx
+++ client/src/components/leadCard.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 
 function LeadCard() {
-    return (
-        <div className="min-h-screen">
-            <div className="container mx-auto mt-8 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
-                <h1 className="text-lg text-center text-white">This is test content</h1>
-                <img src="..." width="300px" height="300px" alt="Test Block" className="mx-auto rounded-lg border-2"/>
-                <p className="text-white pt-4 pb-4 px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, dicta sequi. Rem, dolores debitis quo sunt laboriosam ratione quibusdam! Illo quibusdam iusto deleniti quo esse dolore molestias harum? At, harum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit laudantium omnis natus odio voluptates beatae est rem. Cum odio voluptatum, in, facere voluptate quibusdam sunt nobis sint expedita, dicta repellendus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut recusandae voluptates quasi. Libero labore hic et enim quo modi saepe. Molestias eligendi cumque, tempore facilis molestiae necessitatibus commodi eius nesciunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab eum voluptatibus quisquam fugiat. Libero, cupiditate! Repellendus illo placeat reiciendis exercitationem ab quod ducimus laboriosam explicabo ex enim est, nulla velit?</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="min-h-screen">
+      <div className="container mx-auto mt-8 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
+        <h1 className="text-lg text-center text-white">This is test content</h1>
+        <img
+          src="..."
+          width="300px"
+          height="300px"
+          alt="Test Block"
+          className="mx-auto rounded-lg border-2"
+        />
+        <p className="text-white pt-4 pb-4 px-4">
+          Oh, king eh? Very nice. And how'd you get that, eh? By exploiting the
+          workers. By hanging on to outdated imperialist dogma which perpetuates
+          the economic and social differences in our society. Manacles! Ooooh,
+          my idea of heaven, is to be allowed to be put in manacles. Just for a
+          few hours. They must think the sun shines out your ass, sonny. Shut
+          your festering gob, you tit! Your type really makes me puke you
+          vacuous, toffy-nosed, malodorous pervert! Is your wife a goer, eh?
+          Know what I mean? Know what I mean? Nudge, nudge! Know what I mean?
+          Say no more! Get on with it! Come and see the violence inherent in the
+          system. Help! Help! I'm being repressed! All right, but apart from the
+          sanitation, medicine, education, wine, public order, irrigation,
+          roads, the fresh water system and public health, what have the Romans
+          ever done for us? I cut down trees, I skip and jump, I like to press
+          wildflowers. I put own womens' clothing, and hang around in bars.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LeadCard;
