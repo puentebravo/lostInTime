575ef80c4acf5145a3af1aea2297d5dc6d64589e
Charlie Puente <puentematos@gmail.com>
2022-03-10 19:00:16 -0500

    added social media icons

--- client/src/components/contactCard.tsx
+++ client/src/components/contactCard.tsx
@@ -5,10 +5,10 @@ function ContactCard() {
   return (
     <main className="min-h-screen">
       <section className="container mx-auto mt-8 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
-      <h1 className="text-lg text-center text-white">Follow our adventures!</h1>
-      <table className="grid grid-cols-2 justify-center">
-      <p className="text-white text-center">Twitter</p>
-      <p className="text-white text-center">Instagram</p>
+      <h1 className="text-xl text-center text-white pb-8">Follow our adventures!</h1>
+      <table className="grid grid-cols-2 justify-center pb-4">
+      <p className="text-white text-center"><i className="fa-brands fa-twitter fa-10x"></i></p>
+      <p className="text-white text-center"><i className="fa-brands fa-instagram fa-10x"></i></p>
       </table>
       </section>
     </main>
