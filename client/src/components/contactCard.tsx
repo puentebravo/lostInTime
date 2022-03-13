8e4dc1392e23c8caf9d12f4e37e7c472f65e5f26
Charlie Puente <puentematos@gmail.com>
2022-03-13 17:36:55 -0400

    Added animations to social icons

--- client/src/components/contactCard.tsx
+++ client/src/components/contactCard.tsx
@@ -3,17 +3,21 @@ import React from "react";
 function ContactCard() {
   return (
     <main className="min-h-screen">
-      <section className="container mx-auto mt-8 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
-        <h1 className="text-xl text-center text-white pb-8">
+      <section className="container mx-auto mt-10 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
+        <h1 className="text-xl text-center text-white mb-8 mt-4 pb-4 border-b-2">
           Follow our adventures!
         </h1>
         <div className="grid grid-cols-2 justify-center pb-4">
-          <p className="text-white text-center">
+          <a
+            href="https://twitter.com/puentebravo"
+            className="text-white text-center transition duration-300 ease-in-out hover:text-blue-400"
+            title="Twitter link"
+          >
             <i className="fa-brands fa-twitter fa-10x"></i>
-          </p>
-          <p className="text-white text-center">
+          </a>
+          <a  href="..." className="text-white text-center transition duration-300 ease-in-out hover:text-pink-500" title="Instagram Link">
             <i className="fa-brands fa-instagram fa-10x"></i>
-          </p>
+          </a>
         </div>
       </section>
     </main>
