bb7c917f4914704b7a43f86274c408da5ae19550
Puentebravo <puentematos@gmail.com>
2022-06-09 20:40:56 -0400

    Added social media links to contact card

--- client/src/components/contactCard.tsx
+++ client/src/components/contactCard.tsx
@@ -4,18 +4,18 @@ function ContactCard() {
   return (
     <main className="min-h-screen">
       <section className="container mx-auto mt-10 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
-        <h1 className="text-xl text-center text-white mb-8 mt-4 pb-4 border-b-2">
+        <h1 className="text-xl text-center text-white mb-8 mt-4 pb-4 border-b-2 border-double">
           Follow our adventures!
         </h1>
         <div className="grid grid-cols-2 justify-center pb-4">
           <a
             href="https://twitter.com/puentebravo"
             className="text-white text-center transition duration-300 ease-in-out hover:text-blue-400"
-            title="Twitter link"
+            title="twitter.com/puentebravo"
           >
             <i className="fa-brands fa-twitter fa-10x"></i>
           </a>
-          <a  href="..." className="text-white text-center transition duration-300 ease-in-out hover:text-pink-500" title="Instagram Link">
+          <a  href="..." className="text-white text-center transition duration-300 ease-in-out hover:text-pink-500" title="Instagram link">
             <i className="fa-brands fa-instagram fa-10x"></i>
           </a>
         </div>
