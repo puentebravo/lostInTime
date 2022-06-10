ff46192d1f73689aeb7ccdc18345cd79bbb6d908
Puentebravo <puentematos@gmail.com>
2022-06-09 20:41:50 -0400

    Temporarily commented out state functions

--- client/src/components/blogContainer.tsx
+++ client/src/components/blogContainer.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { getPosts, getTag, getAllTags, Post} from "../utils/API"
 
 function BlogContainer() {
+
+    // const [posts, setPosts] = useState()
+
+    // useEffect( () => {
+    //     getPosts().then( data => {
+    //         setPosts(data)
+    //     })
+    // })
+
+
     return (
-        <div></div>
+        <div className="min-h-screen">
+            <section className="container mx-auto mt-8 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-yellow-400 rounded shadow-lg max-w-3xl min-w-min">
+                <h1>Test</h1>
+            </section>
+        </div>
     )
 }
 
