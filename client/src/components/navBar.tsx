3436e1e338c13f2e133842b1529aff556d26aa23
Puentebravo <puentematos@gmail.com>
2022-06-09 20:52:33 -0400

    Changed navbar color to a darker yellow to comply with branding

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
-        <div className="container mx-auto h-auto position: sticky top-0 bg-yellow-400">
+        <div className="container mx-auto h-auto position: sticky top-0 bg-yellow-500">
             <p className="text-center text-3xl">Lost in Time</p>
             <div className="grid grid-cols-4 justify-center">
                 <Link to="/"><div className="transition duration-300 ease-in-out hover:bg-gray-800 hover:text-white text-center">Home</div></Link>
