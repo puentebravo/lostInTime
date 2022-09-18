58a7162a96c97816c93fbd632d834cdf3af10002
Puentebravo <puentematos@gmail.com>
2022-09-18 14:11:05 -0400

    Added props, type declarations to propCard

--- client/src/components/blogCard.tsx
+++ client/src/components/blogCard.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Post } from "../utils/API";
 
-function BlogCard() {
+function BlogCard(props: Post) {
     return (
-        <div></div>
+        <div className="container">
+            <div className="container mx-auto mt-8 max-h-full bg-gray-600 bg-opacity-90 border-2 border-solid border-black rounded-xl shadow-lg max-w-3xl min-w-min">
+                <div className="flex justify-center">
+                    <h1 className="text-center text-lg text-white">{props.title}</h1>
+                </div>
+                <p className="text-white pt-4 pb-4 px-4">
+                    {props.content}
+                </p>
+                <p className="text-slate-300 text-sm">Tags: {props.tags}</p>
+            </div>
+        </div>
     )
 };
 
